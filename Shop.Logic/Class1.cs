f0a2a9fdb4c4a2097b9a321e81ba5845050ef990
Vadka.casper@gmail.com <Vadka.casper@gmail.com>
2021-10-15 14:45:54 +0700

    Describe the essence of the product

---
+++ Shop.Logic/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Shop.Logic
+{
+    public class Product
+    {
+        public int Id { get; }
+        public string Name { get; }
+
+        public Product(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
