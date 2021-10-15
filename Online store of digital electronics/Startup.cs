4ea4083bea92eed3c34c29abcef19fc061476b5a
Vadka.casper@gmail.com <Vadka.casper@gmail.com>
2021-10-15 13:54:05 +0700

    Adding MVC to a project

--- Online store of digital electronics/Startup.cs
+++ Online store of digital electronics/Startup.cs
@@ -12,13 +12,11 @@ namespace Online_store_of_digital_electronics
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -30,10 +28,9 @@ namespace Online_store_of_digital_electronics
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
