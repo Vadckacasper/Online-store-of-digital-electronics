650bea3d254f32a75914cf50fc909bf20481dc24
Vadckacasper <Vadka.casper@gmail.com>
2021-11-15 16:20:19 +0700

    Redesign of the project structure. Adding a database.

---
+++ Shop.Data/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Shop.Data
+{
+    public class Product
+    {
+        public int Id_product { get; }
+        public int Id_сategory { get; }
+        public int Id_manufacturer { get; }
+        public string Name_product { get; }
+        public string Description { get; }
+        public byte[] Image { get; } //?
+        public decimal Price { get; }
+        public int Available { get; }
+        public string Specifications { get; }
+        public byte Rating { get; }
+
+
+        public Product(int id_product, string name_product)
+        {
+            Id_product = id_product;
+            Name_product = name_product;
+        }
+    }
+}
--- Shop.Data/ProductCatalog.cs
+++ Shop.Data/ProductCatalog.cs
@@ -1,11 +1,11 @@
-﻿using Shop.Logic;
-using System;
+﻿using System;
 using System.Linq;
 
 namespace Shop.Data
 {
     public class ProductCatalog : IProductCatalog
     {
+        private int Id_category { get; }
         private readonly Product[] Products;
         public Product[] GetArrByTitle(string titlePart)
         {
