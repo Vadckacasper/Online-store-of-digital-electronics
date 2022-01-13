90f621f4d154f017fffdf1a96412c1f686d231ff
Kalinin_Vadim <47732165+Vadckacasper@users.noreply.github.com>
2022-01-13 17:00:06 +0700

    Data models added

---
+++ Online store of digital electronics/Models/Manufacturers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Online_store_of_digital_electronics.Models
+{
+    public class Manufacturers
+    {
+        public int Id_manufacturer { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public byte[] Logo { get; set; }
+        public int[] Number_products { get; set; }
+
+
+
+    }
+}
---
+++ Online store of digital electronics/Models/Orders.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Online_store_of_digital_electronics.Models
+{
+    public class Orders
+    {
+    }
+}
