6e9dd3617a55010c31ceaa7a49059196241bbd97
Kalinin_Vadim <47732165+Vadckacasper@users.noreply.github.com>
2022-06-05 17:50:36 +0700

    Fixed the output of products and refined in more detail about the products

--- Online store of digital electronics/Controlles/ViewComponents/SwiperViewComponent.cs
+++ Online store of digital electronics/Controlles/ViewComponents/SwiperViewComponent.cs
@@ -24,6 +24,16 @@ namespace Online_store_of_digital_electronics.ViewComponents
 
         public async Task<IViewComponentResult> InvokeAsync(string nameCategory)
         {
+            string name = nameCategory.Substring(0, nameCategory.Length - 1);
+            string num = nameCategory.Substring(nameCategory.Length-1);
+            int id = 0;
+            if(num == "1")
+            {
+                List<ProductCategory> qwer = _context.productCategories.Include(p => p.Products).Where(c => c.Id_parent != null).ToList();
+                ProductCategory c = qwer.FirstOrDefault(c => c.Name == name);
+                id = qwer.IndexOf(c);
+                nameCategory = qwer[id + 1].Name;
+            }
             string BuyersID = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
             Orders Cart = _context.orders.Include(o => o.ProductOrder).FirstOrDefault(o => o.Buyers.Id == BuyersID && o.Status == "Оформление");
             var ProductCategory = _context.productCategories.Include(p => p.Products).ThenInclude(p => p.manufacturer).FirstOrDefault(c => c.Name == nameCategory);
